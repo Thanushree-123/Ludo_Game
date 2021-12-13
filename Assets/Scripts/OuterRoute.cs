5f02791266389bcd33a2f589bb4725be6f841c78
Thanushree-123 <thanushree196@gmail.com>
2021-12-13 18:40:33 +0530

    GamePlay_Setup

---
+++ Assets/Scripts/OuterRoute.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuterRoute : MonoBehaviour
+{
+    Transform[] childNodes;
+    public List<Transform> childNodesList = new List<Transform>();
+
+    void Start()
+    {
+        FillNodes();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        FillNodes();
+
+        for (int i=0; i<childNodesList.Count; i++)
+        {
+            Vector3 pos = childNodesList[i].position;
+            if(i>0)
+            {
+                Vector3 previous = childNodesList[i-1].position;
+                Gizmos.DrawLine(previous, pos);
+            }
+        }
+    }
+
+    void FillNodes()
+    {
+        childNodesList.Clear();
+
+        childNodes = GetComponentsInChildren<Transform>();
+
+        foreach(Transform child in childNodes)
+        {
+            if(child != this.transform)
+            {
+                childNodesList.Add(child);
+            }
+        }
+
+    }
+}
