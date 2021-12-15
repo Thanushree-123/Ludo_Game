71f28b0d9e36e1fdbb03310a382baab505c6b21f
Thanushree-123 <thanushree196@gmail.com>
2021-12-15 17:59:07 +0530

    Added all 4 players and movement using CPU input

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,14 +42,168 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        if(playerList[activePlayer].playertype == Player.PlayerTypes.CPU)
+        {
         switch(state)
         {
             case States.Roll_Dice:
+            {
+                StartCoroutine(RollDiceDelay());
+                state = States.Waiting;
+            }
             break;
             case States.Waiting:
+            {
+
+            }
             break;
             case States.Switch_Player:
+            {
+                StartCoroutine(SwitchPlayer());
+                state = States.Waiting;
+            }
             break;
         }
+        }
+    }
+
+    void RollDice()
+    {
+        int dicenumber = Random.Range(1,7);
+        //int dicenumber = 6;
+
+        if(dicenumber == 6)
+        {
+            //check the start node
+            CheckStartNode(dicenumber);
+        }
+
+        if(dicenumber < 6)
+        {
+            //check for kick
+            MoveaStone(dicenumber);
+        }
+        Debug.Log("dice rolled number" + dicenumber);
+    }
+
+    IEnumerator RollDiceDelay()
+    {
+        yield return new WaitForSeconds(2);
+        RollDice();
+    }
+
+    void CheckStartNode(int dicenumber)
+    {
+        bool startnodefull = false;
+        for(int i=0; i< playerList[activePlayer].myStones.Length; i++)
+        {
+            if(playerList[activePlayer].myStones[i].currentNode == playerList[activePlayer].myStones[i].startNode)
+            {
+                startnodefull = true;
+                break;
+            }
+        }
+        if(startnodefull)
+        {
+            MoveaStone(dicenumber);
+        }
+        else
+        {
+            for( int i=0; i< playerList[activePlayer].myStones.Length; i++)
+            {
+                if(!playerList[activePlayer].myStones[i].ReturnIsout())
+                {
+                    playerList[activePlayer].myStones[i].LeaveBase();
+                    state = States.Waiting;
+                    return;
+                }
+            }
+            MoveaStone(dicenumber);
+        }
+    }
+
+    void MoveaStone(int dicenumber)
+    {
+        List <Stone> movablestones = new List<Stone>();
+        List <Stone> moveKickstones = new List<Stone>();
+
+        for(int i=0; i< playerList[activePlayer].myStones.Length; i++)
+        {
+            if(playerList[activePlayer].myStones[i].ReturnIsout())
+            {
+                if(playerList[activePlayer].myStones[i].CheckPossibleKick(playerList[activePlayer].myStones[i].stoneid,dicenumber))
+                {
+                    moveKickstones.Add(playerList[activePlayer].myStones[i]);
+                    continue;
+                }
+                if(playerList[activePlayer].myStones[i].CheckPossibleMove(dicenumber))
+                {
+                    movablestones.Add(playerList[activePlayer].myStones[i]);
+                    
+                }
+
+            }
+        }
+
+        if(moveKickstones.Count>0)
+        {
+            int num = Random.Range(0, moveKickstones.Count);
+            moveKickstones[num].StartTheMove(dicenumber);
+            state = States.Waiting;
+            return;
+        }
+
+        if(movablestones.Count>0)
+        {
+            int num = Random.Range(0, movablestones.Count);
+            movablestones[num].StartTheMove(dicenumber);
+            state = States.Waiting;
+            return;
+        }
+        //switching the player
+        state = States.Switch_Player;
+    }
+
+    IEnumerator SwitchPlayer()
+    {
+        if(switchingPlayer)
+        {
+            yield break;
+        }
+
+        switchingPlayer = true;
+        yield return new WaitForSeconds(2);
+        SetNextActivePlayer();
+        switchingPlayer = false;
+    }
+
+    void SetNextActivePlayer()
+    {
+        activePlayer++;
+        activePlayer %= playerList.Count;
+
+        int available = 0;
+        for (int i=0; i< playerList.Count;i++)
+        {
+            if(!playerList[i].hasWon)
+            {
+                available++;
+            }
+        }
+
+        if(playerList[activePlayer].hasWon && available >1)
+        {
+            SetNextActivePlayer();
+            return;
+        }
+        else if(available<2)
+        {
+            //game over screen
+            state = States.Waiting;
+            return;
+        }
+
+        state = States.Roll_Dice;
+
     }
 }
--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
+    public int stoneid;
     [Header("ROUTES")]
     public OuterRoute commonRoute;
     public OuterRoute finalRoute;
@@ -91,6 +92,23 @@ public class Stone : MonoBehaviour
             steps--;
             doneSteps++;
         }
+
+        goalnode = fullRoute[routePosition];
+        if(goalnode.isTaken)
+        {
+            //kick the other stone
+        }
+
+        currentNode.stone = null;
+        currentNode.isTaken = false;
+
+        goalnode.stone = this;
+        goalnode.isTaken = true;
+
+        currentNode = goalnode;
+        goalnode = null;
+
+        GameManager.instance.state = GameManager.States.Switch_Player;
         isMoving = false;
     }
 
@@ -98,6 +116,92 @@ public class Stone : MonoBehaviour
     {
         return goalpos != (transform.position = Vector3.MoveTowards(transform.position,goalpos,speed * Time.deltaTime));
         
+    }
+
+    public bool ReturnIsout()
+    {
+        return isout;
+    }
+
+    public void LeaveBase()
+    {
+        steps =1;
+        isout = true;
+        routePosition = 0;
+
+        StartCoroutine(MoveOut());
+    }
+
+    IEnumerator MoveOut()
+    {
+        if(isMoving)
+        {
+            yield break;
+        }
+
+        isMoving = true;
+
+        while(steps>0)
+        {
+            //routePosition++;
+            Vector3 nextPos = fullRoute[routePosition].gameObject.transform.position;
+            while(MoveToNextNode(nextPos,8f))
+            {
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.1f);
+            steps--;
+            doneSteps++;
+        }
+
+        goalnode = fullRoute[routePosition];
+        if(goalnode.isTaken)
+        {
+
+        }
+
+        goalnode.stone = this;
+        goalnode.isTaken = true;
+
+        currentNode = goalnode;
+        goalnode = null;
+ 
+        GameManager.instance.state = GameManager.States.Roll_Dice;
+        isMoving = false;
+    }
+
+    public bool CheckPossibleMove(int dicenumber)
+    {
+        int tempos = routePosition + dicenumber;
+        if(tempos >= fullRoute.Count)
+        {
+            return false;
+        }
+        return !fullRoute[tempos].isTaken;
+    }
+
+    public bool CheckPossibleKick(int stoneID, int dicenumber)
+    {
+        int tempos = routePosition + dicenumber;
+        if(tempos >= fullRoute.Count)
+        {
+            return false;
+        }
+        if(fullRoute[tempos].isTaken)
+        {
+            if(stoneID == fullRoute[tempos].stone.stoneid)
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void StartTheMove(int dicenumber)
+    {
+        steps = dicenumber;
+        StartCoroutine(Move());
     }
 
 
